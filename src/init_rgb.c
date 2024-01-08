b24c0404b951e2e34704995bb231dc34773341a8
cedmulle <cedmulle@student.42lausanne.ch>
2024-01-08 20:09:38 +0100

    fixed norme

--- src/init_rgb.c
+++ src/init_rgb.c
@@ -6,7 +6,7 @@
 /*   By: cedmulle <cedmulle@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 19:46:58 by cedmulle          #+#    #+#             */
-/*   Updated: 2024/01/08 20:04:04 by cedmulle         ###   ########.fr       */
+/*   Updated: 2024/01/08 20:09:25 by cedmulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ void	init_rgb(t_game *game)
 	{
 		game->settings->fl_rgb[0] = ft_atoi(game->settings->floor);
 		game->settings->fl_rgb[1] = ft_atoi(ft_strchr(game->settings->floor,
-			',') + 1);
+					',') + 1);
 		game->settings->fl_rgb[2] = ft_atoi(ft_strrchr(game->settings->floor,
-			',') + 1);
+					',') + 1);
 	}
 	if (game->settings->cl_ispath == false)
 	{
 		game->settings->cl_rgb[0] = ft_atoi(game->settings->ceil);
 		game->settings->cl_rgb[1] = ft_atoi(ft_strchr(game->settings->ceil,
-			',') + 1);
+					',') + 1);
 		game->settings->cl_rgb[2] = ft_atoi(ft_strrchr(game->settings->ceil,
-			',') + 1);
+					',') + 1);
 	}
 }
